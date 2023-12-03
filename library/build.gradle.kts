a36b100a5628afb8cca57f26ab8451630d2ee299
vadwwxz <vadwwxz@gmail.com>
2023-12-03 16:30:34 +0200

    Initial commit

---
+++ library/build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+}
+
+kotlin {
+    explicitApi = ExplicitApiMode.Strict
+    jvmToolchain(11)
+
+    androidTarget()
+    jvm()
+    js {
+        browser()
+        nodejs()
+    }
+
+    sourceSets {
+        val commonMain by getting
+        val jvmMain by getting
+        val jsMain by getting
+        val androidMain by getting
+    }
+}
+
+android {
+    namespace = "com.sliderzxc.aurum.android"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
